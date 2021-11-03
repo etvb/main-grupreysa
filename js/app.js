08e563636ee1ae8a964d67e91392b727c3cfeea2
etvb <edgardo.torres@hotmail.com>
2021-11-03 11:45:49 -0600

    adding functionality in the nav

---
+++ js/app.js
@@ -0,0 +1,8 @@
+function show() {
+    document.getElementById('slidingMenu').classList.add('open');
+}
+
+function hide() {
+    console.log('h');
+    document.getElementById('slidingMenu').classList.remove('open');
+}
