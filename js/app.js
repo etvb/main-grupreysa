b41a5ab6b582131c0b8d13b65c6221642b82e28b
etvb <edgardo.torres@hotmail.com>
2022-08-12 08:14:22 -0500

    add mission, vision and values

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
+let card = document.getElementById('1');
+let cardtoggle = document.getElementById('1.1');
+
+let card2 = document.getElementById('2');
+let cardtoggle2 = document.getElementById('2.1');
+
+let card3 = document.getElementById('3');
+let cardtoggle3 = document.getElementById('3.1');
+
+cardtoggle.onclick = function(){
+    card.classList.toggle('active-110822');
+}
+
+cardtoggle2.onclick = function(){
+    card2.classList.toggle('active-110822');
+}
+
+cardtoggle3.onclick = function(){
+    card3.classList.toggle('active-110822');
+}
+
+
 function show() {
     document.getElementById('slidingMenu').classList.add('open');
 }
